a53460af012929c8a51d7098656b90a1e01c5f13
higogomes <higogomes@gmail.com>
2021-10-26 21:24:51 -0300

    Atualizacao sem pdf

--- conf.py
+++ conf.py
@@ -106,7 +106,7 @@ htmlhelp_basename = 'DUVIDASFREQUENTESCONTAGOVBR'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
+#latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
@@ -127,9 +127,9 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'ManualScriptChatbotLoginnico.tex', u'Dúvidas Frequentes da Conta gov.br',
-     u'DESPD', 'manual'),
+#latex_documents = [
+#    (master_doc, 'ManualScriptChatbotLoginnico.tex', u'Dúvidas Frequentes da Conta gov.br',
+#     u'DESPD', 'manual'),
 ]
 
 
